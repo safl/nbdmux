ec34f45147bbd3ba14bf047961b9bc6022d7e415
Simon A. F. Lund <os@safl.dk>
2026-07-04 07:34:40 +0200

    chore: v0.2.11
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- src/nbdmux/__init__.py
+++ src/nbdmux/__init__.py
@@ -18,7 +18,7 @@ nbd-server``; Fedora: ``dnf install nbd``).
 
 from .client import add_export, is_healthy, list_exports, remove_export, warm_export
 
-__version__ = "0.2.10"
+__version__ = "0.2.11"
 
 __all__ = [
     "__version__",
